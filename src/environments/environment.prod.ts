0e907b4b9a9cfb3353638f7a40ee578d66ca7c13
Imanplay23 <yandisanchez0321@gmail.com>
2026-09-04 22:55:24 -0400

    feat: base de datos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_AUTH_DOMAIN',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_STORAGE_BUCKET',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId: 'YOUR_APP_ID'
+    apiKey: 'AIzaSyBnMVtY04gCozlzFkIN8n8Q2J_sDyhqPH0',
+    authDomain: 'planify-5c91a.firebaseapp.com',
+    projectId: 'planify-5c91a',
+    storageBucket: 'planify-5c91a.firebasestorage.app',
+    messagingSenderId: '414624219252',
+    appId: '1:414624219252:web:7dfec0c1d46f11f8e99121',
+    measurementId: 'G-41FECZ056G'
   }
 };
